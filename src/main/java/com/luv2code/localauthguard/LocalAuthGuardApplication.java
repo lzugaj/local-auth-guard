2c35e6e30f30c7efd65aa0726a38d52a61c0ab8f
lzugaj <luka.zugaj@sigit.hr>
2024-09-29 17:31:17 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/localauthguard/LocalAuthGuardApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.localauthguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalAuthGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalAuthGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv2code/localauthguard/LocalAuthGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.localauthguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalAuthGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
